04d645d89f960656fcda1112b82ad4ee94d46568
harshad maniyar <maniyar.harshad92@gmail.com>
2024-03-31 22:40:46 +0530

    code clean

--- src/main/java/org/my/hakuna/MyApplication.java
+++ src/main/java/org/my/hakuna/MyApplication.java
@@ -9,16 +9,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class MyApplication {
 
 	public static void main(String[] args) {
-//		System.out.println(generateSafeToken()); 
 		SpringApplication.run(MyApplication.class, args);
 	}
 	
-//	private static String generateSafeToken() {
-//	    SecureRandom random = new SecureRandom();
-//	    byte[] bytes = new byte[36]; // 36 bytes * 8 = 288 bits, a little bit more than
-//	                                 // the 256 required bits 
-//	    random.nextBytes(bytes);
-//	    var encoder = Base64.getUrlEncoder().withoutPadding();
-//	    return encoder.encodeToString(bytes);
-//	}
 }
